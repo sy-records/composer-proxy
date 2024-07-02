f6d00a93d190542dc47ab6849edded19dc35bbf1
Lufei <lufei@easycorp.ltd>
2024-07-02 15:18:37 +0800

    Add zenboard proxy

---
+++ main.php
@@ -0,0 +1,40 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Symfony\Component\Finder\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->path('/^[\w-]+\/composer\.lock/');
+
+$url      = getenv("CODING_URL");
+$login    = getenv("CODING_LOGIN");
+$password = getenv("CODING_PASSWORD");
+
+foreach ($finder as $value) {
+    $json = json_decode(
+        file_get_contents($value->getPathname()),
+        true
+    );
+
+    $packages = $json['packages'] ?? [];
+    foreach ($packages as $package) {
+        $name       = $package['name'] ?? null;
+        $version    = $package['version'] ?? null;
+        $packageUrl = $package['dist']['url'] ?? null;
+
+        if ($name && $version && $packageUrl) {
+            $dir     = dirname($name);
+            $realDir = dirname($value->getRealPath()) . '/vendor/' . $name;
+            @mkdir(__DIR__ . '/build/' . $dir, 0777, true);
+
+            $build = __DIR__ . '/build/' . $name . '.zip';
+            $cmd   = "cd {$realDir} && zip -r {$build} .";
+            @exec($cmd);
+
+            $cmd = "cd build && curl -T {$name}.zip -u {$login}:{$password} {$url}?version={$version}";
+            @exec($cmd);
+        }
+    }
+}
